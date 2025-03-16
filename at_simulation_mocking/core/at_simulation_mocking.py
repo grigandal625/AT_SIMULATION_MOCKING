f362936b052791cc9f1e31986af4858905af4356
grigadnal625 <grigandal625@gmail.com>
2025-03-16 15:51:44 +0300

    update methods

--- at_simulation_mocking/core/at_simulation_mocking.py
+++ at_simulation_mocking/core/at_simulation_mocking.py
@@ -5,13 +5,30 @@ from at_queue.core.at_component import ATComponent
 from at_queue.core.session import ConnectionParameters
 from at_queue.utils.decorators import authorized_method
 from at_simulation_mocking.core.at_rao_structs import SMRun, TactDict, ResourceMPDict
-from typing import Dict, Union, List
+from typing import Dict, Union, List, TypedDict, Literal
 import logging
 import json
 from xml.etree.ElementTree import fromstring, Element
+from uuid import uuid4
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
+class ProcessDict(TypedDict):
+    id: str
+    name: str
+    file_id: str
+    status: Literal['PAUSE', 'RUNNING', 'KILLED']
+    current_tick: int
+
+
+class TranslatedFileDict(TypedDict):
+    id: str
+    name: str
+    model_id: int
+    created_at: datetime
+    size: int
+    model_name: str
 
 class SM_LOAD_MODE:
     JSON = 'json'
@@ -20,12 +37,23 @@ class SM_LOAD_MODE:
 
 class ATSimulationMocking(ATComponent):
     sm_runs: Dict[str, SMRun]
-    last_tacts: Dict[str, int]
+    process_mockings: Dict[str, List[ProcessDict]]
+    translated_files: List[TranslatedFileDict]
 
     def __init__(self, connection_parameters: ConnectionParameters, *args, **kwargs):
         super().__init__(connection_parameters, *args, **kwargs)
         self.sm_runs = {}
-        self.last_tacts = {}
+        self.process_mockings = {}
+        self.translated_files = [
+            {
+                "id": str(uuid4()),
+                "name": 'File 1',
+                "model_id": 1,
+                "created_at": datetime.now(),
+                "size": 1024,
+                "model_name": "Unknown",
+            }
+        ]
 
     async def perform_configurate(self, config: ATComponentConfig, auth_token: str = None, *args, **kwargs) -> bool:
         auth_token = auth_token or 'default'
@@ -73,29 +101,60 @@ class ATSimulationMocking(ATComponent):
             raise ValueError("Simulation model run for token '%s' is not created" % auth_token)
         return sm_run
     
-    def get_last_tact(self, auth_token) -> int:
+    def get_process_mocking(self, auth_token, process_id) -> ProcessDict:
         auth_token = auth_token or 'default'
-        last_tact = self.last_tacts.get(auth_token)
-        if last_tact is None:
-            last_tact = -1
-            self.last_tacts[auth_token] = last_tact
-        return last_tact
+        process_mockings = self.process_mockings.get(auth_token, [])
+        process_mocking = next(iter([p for p in process_mockings if p['id'] == process_id]), None)
+        if process_mocking is None:
+            raise ValueError(f"No processes created for provided token with id {process_id}")
+        return process_mocking
+        
     
     @authorized_method
-    def reset(self, auth_token: str = None) -> bool:
+    def get_translated_files(self, auth_token: str = None) -> List[TranslatedFileDict]:
+        return self.translated_files
+    
+    @authorized_method
+    def create_process(self, auth_token: str = None, **kwargs) -> ProcessDict:
         auth_token = auth_token or 'default'
-        self.last_tacts[auth_token] = 0
+        processes = self.process_mockings.get(auth_token, [])
+        processes.append({
+            "id": str(uuid4()),
+            "name": kwargs.get('process_name') or 'process_name',
+            "file_id": kwargs.get('file_id') or str(uuid4()),
+            "status": 'PAUSE',
+            "current_tick": 0,
+        })
+        self.process_mockings[auth_token] = processes
+        return processes[-1] 
+    
+    @authorized_method
+    def kill_process(self, process_id: str, auth_token: str = None, **kwargs) -> None:
+        auth_token = auth_token or 'default'
+        process = self.get_process_mocking(auth_token, process_id)
+        process['status'] = 'KILLED'
+        return process
 
     @authorized_method
-    def run_tick(self, recycle: bool = True, auth_token: str = None, **kwargs) -> List[ResourceMPDict]:
+    def run_tick(self, process_id: str, recycle: bool = True, auth_token: str = None, **kwargs) -> List[ResourceMPDict]:
         sm_run = self.get_sm_run(auth_token)
-        new_tact = self.get_last_tact(auth_token) + 1
+        process = self.get_process_mocking(auth_token, process_id)
+        if process['status'] == 'KILLED':
+            raise ValueError('Can not run tick of killed process')
+        current_tact = process['current_tick']
+        new_tact = current_tact + 1
         if new_tact > sm_run.max_tact:
             if not recycle:
                 raise ValueError(f'Trying to process tact {new_tact} but max tact is {sm_run.max_tact}')
         
         sm_tact = new_tact % (sm_run.max_tact + 1)
-        self.last_tacts[auth_token] = new_tact
+        process['current_tick'] = new_tact
         return sm_run.__dict__[sm_tact]
+    
+    @authorized_method
+    def get_processes(self, auth_token: str = None, **kwargs) -> List[ProcessDict]:
+        auth_token = auth_token or 'default'
+        return self.process_mockings.get(auth_token, [])
+
 
     
--- tests/test_loading_and_running.py
+++ tests/test_loading_and_running.py
@@ -18,13 +18,62 @@ def xml_sm_run():
 def test_load_and_run_from_xml(xml_sm_run):
     mocking = ATSimulationMocking(connection_parameters=ConnectionParameters())
     mocking.create_sm_run(xml_sm_run, mode='at4_xml')
-    tick = mocking.run_tick(auth_token='default', process_id='test-proc-id')
-    assert 'resources' in tick
     
+    processes = mocking.get_processes(auth_token='default')
+    assert isinstance(processes, list)
+    assert not len(processes)
+    process = mocking.create_process(auth_token='default')
+    assert 'id' in process
 
+    process_id = process['id']
+    processes = mocking.get_processes(auth_token='default')
+    assert isinstance(processes, list)
+    assert len(processes) == 1
+    assert processes[0]['id'] == process_id
+    assert processes[0]['status'] == 'PAUSE'
+
+    tick = mocking.run_tick(auth_token='default', process_id=process_id)
+    assert isinstance(tick, dict)
+    assert 'resources' in tick
+
+    tick = mocking.run_tick(auth_token='default', process_id=process_id)
+    assert isinstance(tick, dict)
+    assert 'resources' in tick
+
+    process = mocking.kill_process(process_id=process_id, auth_token='default')
+    assert isinstance(process, dict)
+    assert 'id' in process
+    assert process['id'] == process_id
+    assert process['status'] == 'KILLED'
+    
 
 def test_load_and_run_from_json(json_sm_run):
     mocking = ATSimulationMocking(connection_parameters=ConnectionParameters())
     mocking.create_sm_run(json_sm_run, mode='json')
-    tick = mocking.run_tick(auth_token='default', process_id='test-proc-id')
+    
+    processes = mocking.get_processes(auth_token='default')
+    assert isinstance(processes, list)
+    assert not len(processes)
+    process = mocking.create_process(auth_token='default')
+    assert 'id' in process
+
+    process_id = process['id']
+    processes = mocking.get_processes(auth_token='default')
+    assert isinstance(processes, list)
+    assert len(processes) == 1
+    assert processes[0]['id'] == process_id
+    assert processes[0]['status'] == 'PAUSE'
+
+    tick = mocking.run_tick(auth_token='default', process_id=process_id)
+    assert isinstance(tick, dict)
     assert 'resources' in tick
+
+    tick = mocking.run_tick(auth_token='default', process_id=process_id)
+    assert isinstance(tick, dict)
+    assert 'resources' in tick
+
+    process = mocking.kill_process(process_id=process_id, auth_token='default')
+    assert isinstance(process, dict)
+    assert 'id' in process
+    assert process['id'] == process_id
+    assert process['status'] == 'KILLED'
